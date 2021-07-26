bbef1ebec1c5c5529e3704667cefb4830ceb4a88
qq1065507891 <42400109+qq1065507891@users.noreply.github.com>
2021-07-26 15:09:00 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,20 @@
-from bert4keras.backend import keras, set_gelu
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Lambda, Dense, Dropout
+import os
+os.environ['TF_KERAS'] = '1'
+
+from bert4keras.backend import set_gelu
 from bert4keras.models import build_transformer_model
-from keras.layers import Lambda, Dense, Dropout
 
 
-class Bert_classifier():
+class BertClassifier(object):
     def __init__(self, config, last_activation='softmax', dropout_rate=0):
         self.config = config
         self.last_activation = last_activation
         self.dropout_rate = dropout_rate
 
     def build_model(self):
-        set_gelu('tanh')  # 切换gelu版本
+        set_gelu('tanh')
         # 加载预训练模型
         bert = build_transformer_model(
             config_path=self.config['config_path'],
@@ -21,14 +25,21 @@ class Bert_classifier():
 
         output = Lambda(lambda x: x[:, 0], name='CLS-token')(bert.model.output)
 
+        # dorpout层
         if 0 < self.dropout_rate < 1:
             output = Dropout(self.dropout_rate)(output)
+
         output = Dense(
             units=self.config['num_classes'],
-            activation='softmax',
+            activation=self.last_activation,
             kernel_initializer=bert.initializer
         )(output)
 
-        model = keras.models.Model(bert.model.input, output)
-        # model.summary()
+        model = Model(bert.model.input, output)
+#         model.summary()
         return model
+
+if __name__ == '__main__':
+    from config import config
+    model = BertClassifier(config, dropout_rate=0.2).build_model()
+    model.summary()
